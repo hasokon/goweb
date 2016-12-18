1923d415bbc5c824a408c32c6912c26fef063677
kentaro@chromebook <kentaro.linux@gmail.com>
2016-12-18 15:11:26 +0900

    This is 1.3 Code
    This Program is excutable as Chat Web Apprication for now

--- chat/main.go
+++ chat/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
-	"text/template"
 	"path/filepath"
 	"sync"
+	"text/template"
 )
 
 type templateHandler struct {
-	once sync.Once
+	once     sync.Once
 	filename string
-	temp1 *template.Template
+	temp1    *template.Template
 }
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -19,11 +19,15 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.temp1 =
 			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.temp1.Execute(w,nil)
+	t.temp1.Execute(w, nil)
 }
 
 func main() {
-	http.Handle("/", &templateHandler{filename : "chat.html"})
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	go r.run()
 
 	// Start Web Server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
