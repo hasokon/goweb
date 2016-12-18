10d81e45660e1e255c37cbe86d9568024c885158
kentaro@chromebook <kentaro.linux@gmail.com>
2016-12-18 13:36:01 +0900

    This is 1.1 Code
    Using Templates

--- chat/main.go
+++ chat/main.go
@@ -3,21 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title> Let's Chat! </title>
-				</head>
-				<body>
-					Let's Chat!
-				</body>
-			</html>
-			`))
+type templateHandler struct {
+	once sync.Once
+	filename string
+	temp1 *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.temp1 =
+			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.temp1.Execute(w,nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename : "chat.html"})
 
 	// Start Web Server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
